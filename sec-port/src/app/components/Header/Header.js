78ec12b82bde01e9923ab56df783c1c10960ae0e
Chuma-rxt <craxoti60@gmail.com>
2024-03-26 13:57:30 +0200

    creating Navbar

--- sec-port/src/app/components/Header/Header.js
+++ sec-port/src/app/components/Header/Header.js
@@ -13,8 +13,102 @@ export default function Header() {
             setIsScrolled(scrollTop > 0)
         }
         window.addEventListener("scroll", handleScroll)
-    })
+
+        return()=>{
+            window.removeEventListener("scroll", handleScroll)
+        }
+    },[])
+
+    console.log(isScrolled) 
   return (
-    <div>Header</div>
-  )
+        <React.Fragment>
+            <header className={`${isScrolled ? "headerShow" : ""} fixed top-0 z-50 transition-all duration-500`}
+            style={{
+                backgroundColor: isScrolled ? "#fff" : "transparent",
+                boxShadow: isScrolled ? "#48AFDE -10px 25px 50px 10px" : "",
+            }}
+            >
+                <nav className="invisible xl:visible xl:max-w-4xl 2xl:max-w-7xl mx-auto ">
+                    <ul className="flex font-recoletaBlack flex-row items-center h-24">
+                       <li className="group text-2xl relative font-bold mr-20" >
+                        {selectedIndex1 === 0 ?(
+                            <span className="menu-effect transform opacity-100 -rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span>
+                        ):(
+                            <span className="menu-effect transform opacity-100 rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span> 
+                        )}
+
+                            <a className={`menu-item ${selectedIndex1 ===0 ? "text-black":""} text-[#666d47] group-hover:text-black`}
+                            href="/#home"
+                            onClick={()=> setSelectedIndex1(0)}
+                            >
+                                Home
+                            </a>
+                       </li>
+
+                       <li className="group text-2xl relative font-bold mr-20" >
+                        {selectedIndex1 === 1 ?(
+                            <span className="menu-effect transform opacity-100 -rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span>
+                        ):(
+                            <span className="menu-effect transform opacity-100 rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span> 
+                        )}
+
+                            <a className={`menu-item ${selectedIndex1 ===0 ? "text-black":""} text-[#666d47] group-hover:text-black`}
+                            href="/#portfolio"
+                            onClick={()=> setSelectedIndex1(1)}
+                            >
+                                Portfolio
+                            </a>
+                       </li>
+
+                       <li className="group text-2xl relative font-bold mr-20" >
+                        {selectedIndex1 === 2 ?(
+                            <span className="menu-effect transform opacity-100 -rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span>
+                        ):(
+                            <span className="menu-effect transform opacity-100 rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span> 
+                        )}
+
+                            <a className={`menu-item ${selectedIndex1 ===0 ? "text-black":""} text-[#666d47] group-hover:text-black`}
+                            href="/#about-me-component"
+                            onClick={()=> setSelectedIndex1(2)}
+                            >
+                                AboutMe
+                            </a>
+                       </li>
+
+                       <li className="group text-2xl relative font-bold mr-20" >
+                        {selectedIndex1 === 3 ?(
+                            <span className="menu-effect transform opacity-100 -rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span>
+                        ):(
+                            <span className="menu-effect transform opacity-100 rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span> 
+                        )}
+
+                            <a className={`menu-item ${selectedIndex1 ===0 ? "text-black":""} text-[#666d47] group-hover:text-black`}
+                            href="/#home"
+                            onClick={()=> setSelectedIndex1(3)}
+                            >
+                               Hire Me
+                            </a>
+                       </li>
+
+                       <li className="group text-2xl relative font-bold mr-20" >
+                        {selectedIndex1 === 0 ?(
+                            <span className="menu-effect transform opacity-100 -rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span>
+                        ):(
+                            <span className="menu-effect transform opacity-100 rotate-12 group-hover:-rotate-12 group-hover:opacity-100"></span> 
+                        )}
+
+                            <a className={`menu-item ${selectedIndex1 ===0 ? "text-black":""} text-[#666d47] group-hover:text-black`}
+                            href="/#home"
+                            onClick={()=> setSelectedIndex1(0)}
+                            >
+                                Home
+                            </a>
+                       </li>
+                    </ul>
+                </nav>
+                {/* <h1 className="text-7xl">Headeer</h1> */}
+
+            </header>
+        </React.Fragment>
+    );
 }
